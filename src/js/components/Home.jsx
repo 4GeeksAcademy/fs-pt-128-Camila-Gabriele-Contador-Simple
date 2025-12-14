6ca56e6753b8f974c674b72082a0ccf6d7cdb7c0
Camila Gabriele <camilagabriele@outlook.com.ar>
2025-12-14 14:43:01 +0000

    se agrega fondo y estilos

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -9,14 +9,16 @@ const Home = ({ count }) => {
 
 	return (
 		<>
+			<h1 className="text-white d-flex justify-content-center align-item-center mt-4">MONACO GRAND PRIX</h1>
+			<h2 className="text-danger d-flex justify-content-center align-item-center"> 175 DAYS LEFT </h2>
 			<div classNameName="botones position-absolute top-50 start-50">
 				<div className="btn-group">
-					<button type="button" className="btn btn-outline-danger icono"><i className="fa-regular fa-clock"></i></button>
-					<button type="button" className="btn btn-outline-danger mid">{Math.floor(count / 10000) % 10} </button>
-					<button type="button" className="btn btn-outline-danger mid">{Math.floor(count / 1000) % 10} </button>
-					<button type="button" className="btn btn-outline-danger mid">{Math.floor(count / 100) % 10} </button>
-					<button type="button" className="btn btn-outline-danger mid">{Math.floor(count / 10) % 10}</button>
-					<button type="button" className="btn btn-outline-danger end">{Math.floor(count / 1) % 10} </button>
+					<button type="button" className="btn btn-outline-light border-5 icono"><i className="fa-regular fa-clock"></i></button>
+					<button type="button" className="btn btn-outline-light border-5 mid">{Math.floor(count / 10000) % 10} </button>
+					<button type="button" className="btn btn-outline-light border-5 mid">{Math.floor(count / 1000) % 10} </button>
+					<button type="button" className="btn btn-outline-light border-5 mid">{Math.floor(count / 100) % 10} </button>
+					<button type="button" className="btn btn-outline-light border-5 mid">{Math.floor(count / 10) % 10}</button>
+					<button type="button" className="btn btn-outline-light border-5 end">{Math.floor(count / 1) % 10} </button>
 				</div>
 			</div>
 		</>
